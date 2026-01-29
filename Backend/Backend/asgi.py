58d9a18cecb3edf9753819aae1141bd038145f1d
Muhammed Shan <muhammedshan930o@gmail.com>
2026-01-30 03:15:30 +0530

    chat bot websocket all roll back to normal state and all webscket corrected the path

--- Backend/Backend/asgi.py
+++ Backend/Backend/asgi.py
@@ -2,8 +2,6 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from channels.security.websocket import OriginValidator
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend.settings")
 
@@ -13,16 +11,12 @@ from apps.groups.routing import websocket_urlpatterns as chat_wb
 from apps.transcription_notes.routing import websocket_urlpatterns as live_transcription_ws
 from apps.chat_bot.routing import websocket_urlpatterns as chat_bot_ws
 from apps.accounts.middleware import JWTAuthMiddleware
-print("CHAT BOT WS:", chat_bot_ws)
-
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": JWTAuthMiddleware(
-        URLRouter(
-            chat_wb +
-            live_transcription_ws +
-            chat_bot_ws
+    "websocket": AllowedHostsOriginValidator(
+        JWTAuthMiddleware(
+            URLRouter(chat_wb + live_transcription_ws + chat_bot_ws)
         )
     ),
 })
